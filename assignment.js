19939e040ee6af9499d324041262cfbf2aa36cca
Abhinav S <Abhinav.S@BRILLIO.COM>
2023-06-19 23:06:27 +0530

    Day 1 June 19 2023 assignment started while doing videos

---
+++ assignment.js
@@ -0,0 +1,12 @@
+let country = "India";
+let continent = "Asia";
+let population = 140;
+let isIsland = false;
+let language;
+
+console.log(
+  typeof isIsland,
+  typeof population,
+  typeof country,
+  typeof language
+);
